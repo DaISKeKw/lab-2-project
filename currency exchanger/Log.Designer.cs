56e7e115449d8467febbd8bc7db373184cd20950
DaIS <DaIS@HOME-PC>
2023-04-22 02:30:46 +0300

    1

--- currency exchanger/Log.Designer.cs
+++ currency exchanger/Log.Designer.cs
@@ -58,9 +58,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 678);
+            this.ClientSize = new System.Drawing.Size(472, 678);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.LOG1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
